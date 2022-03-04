575e02de3f1273e1afe58cb8dbab70853f7de787
Kaumil Patel <spdmode@gmail.com>
2022-03-03 23:18:53 -0700

    Valid Rows and Column Tests

--- JFreeChart_Lab3/src/org/jfree/data/test/DataUtilitiesTest_v2.java
+++ JFreeChart_Lab3/src/org/jfree/data/test/DataUtilitiesTest_v2.java
@@ -636,4 +636,50 @@ public class DataUtilitiesTest_v2 {
 		assertEquals("Empty input should return 0", 0, result, .01d);
 	}
 
+	// calculateColumnTotal validRows
+	
+	@Test
+	public void calculateColumnTotalValidRowsForNullValue() {
+		int[] validRows = {0,2};
+		Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {
+			{
+				oneOf(values).getRowCount();
+				will(returnValue(3));
+				oneOf(values).getValue(0, 0);
+				will(returnValue(null));
+				oneOf(values).getValue(1, 0);
+				will(returnValue(2.5));
+				oneOf(values).getValue(2, 0);
+				will(returnValue(1.5));
+
+			}
+		});
+		double result = DataUtilities.calculateColumnTotal(values, 0, validRows);
+		assertEquals(1.5, result, .01d);
+	}
+	
+	// calculateRowTotal validColumns
+	
+	@Test
+	public void calculateRowTotalValidColumnsForNullValue() {
+		int[] validColumns = {0,2};
+		Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {
+			{
+				oneOf(values).getColumnCount();
+				will(returnValue(3));
+				oneOf(values).getValue(0, 0);
+				will(returnValue(null));
+				oneOf(values).getValue(0, 1);
+				will(returnValue(2.5));
+				oneOf(values).getValue(0, 2);
+				will(returnValue(1.5));
+
+			}
+		});
+		double result = DataUtilities.calculateRowTotal(values, 0, validColumns);
+		assertEquals(1.5, result, .01d);
+	}
+	
 }
